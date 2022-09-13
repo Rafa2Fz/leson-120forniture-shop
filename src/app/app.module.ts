20c8f4610de243780d61edd67c36308bc05099f4
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-09-13 14:14:46 -0300

    Implemeted a product list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PresentationComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
